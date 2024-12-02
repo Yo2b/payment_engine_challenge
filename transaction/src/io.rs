325ec5b17db5e4db818c3b00fb7e5acc6cca989a
Yoann LAMBERT <y.lambert@jellysmack.com>
2024-12-02 19:05:41 +0100

    feat(transaction): implement CSV (de)serialization in `io` module

--- transaction/src/io.rs
+++ transaction/src/io.rs
@@ -1,11 +1,11 @@
 //! A module providing transaction I/O features.
 
-use csv_async::{AsyncReader, AsyncReaderBuilder, AsyncWriter, AsyncWriterBuilder, Error, Trim};
+use csv_async::{AsyncDeserializer, AsyncReaderBuilder, AsyncSerializer, AsyncWriterBuilder, Error, Trim};
 use futures::stream::TryStreamExt;
 use tokio::io;
 
 /// Configure a CSV reader to initiate a transaction process.
-pub fn reader(rdr: impl io::AsyncRead + Send + Unpin) -> io::Result<AsyncReader<impl io::AsyncRead>> {
+pub fn reader(rdr: impl io::AsyncRead + Send + Unpin) -> io::Result<AsyncDeserializer<impl io::AsyncRead>> {
     // let rdr = io::BufReader::new(rdr); // CSV reader is already buffered
 
     let reader = AsyncReaderBuilder::default()
@@ -13,34 +13,32 @@ pub fn reader(rdr: impl io::AsyncRead + Send + Unpin) -> io::Result<AsyncReader<
         .end_on_io_error(true)
         .has_headers(true)
         .flexible(false)
-        .create_reader(rdr);
+        .create_deserializer(rdr);
 
     Ok(reader)
 }
 
 /// Configure a CSV writer to initiate a transaction process.
-pub fn writer(wtr: impl io::AsyncWrite + Unpin) -> io::Result<AsyncWriter<impl io::AsyncWrite>> {
+pub fn writer(wtr: impl io::AsyncWrite + Unpin) -> io::Result<AsyncSerializer<impl io::AsyncWrite>> {
     let writer = AsyncWriterBuilder::default()
         .has_headers(true)
         .flexible(false)
-        .create_writer(wtr);
+        .create_serializer(wtr);
 
     Ok(writer)
 }
 
 /// Run a transaction process.
-pub async fn process<R, W>(mut reader: AsyncReader<R>, mut writer: AsyncWriter<W>) -> Result<(), Error>
+pub async fn process<R, W>(reader: AsyncDeserializer<R>, mut writer: AsyncSerializer<W>) -> Result<(), Error>
 where
     R: io::AsyncRead + Send + Unpin,
     W: io::AsyncWrite + Unpin,
 {
-    let headers = reader.byte_headers().await?;
-    writer.write_byte_record(headers).await?;
+    let mut stream = reader.into_deserialize::<Vec<String>>();
 
-    let mut stream = reader.byte_records();
     while let Some(record) = stream.try_next().await? {
         tracing::debug!("{record:?}");
-        writer.write_byte_record(&record).await?;
+        writer.serialize(record).await?;
     }
 
     writer.flush().await?;
