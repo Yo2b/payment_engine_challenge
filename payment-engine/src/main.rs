93962d1dfe4af27fcd2c3e83edc7d927e5280f31
Yoann LAMBERT <y.lambert@jellysmack.com>
2024-12-02 13:16:05 +0100

    feat(payment-engine): add basic CLI args and logger

--- payment-engine/src/main.rs
+++ payment-engine/src/main.rs
@@ -1,1 +1,25 @@
-fn main() {}
+use std::path::PathBuf;
+
+use clap::Parser;
+use tracing_subscriber::{fmt, EnvFilter};
+
+/// Struct to register all CLI args.
+#[derive(Debug, Parser)]
+#[command(about = "A simple toy payments engine!")]
+struct Cli {
+    /// The payment inputs as a path to a valid CSV file
+    input_file_path: PathBuf,
+}
+
+fn main() {
+    // Install logger
+    fmt::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_writer(std::io::stderr)
+        .init();
+
+    // Parse CLI args
+    let cli = Cli::parse();
+
+    tracing::info!("Processing payments from input file: `{}`", cli.input_file_path.display());
+}
